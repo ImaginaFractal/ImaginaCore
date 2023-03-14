f2b7f9f571c124aa39eaf479a07cb09192c31db3
Zhuoran <yuzhuoran13@gmail.com>
2023-03-14 08:07:38 +0800

    StandardEvaluator and LowPrecisionEvaluator.

--- Evaluator.cpp
+++ Evaluator.cpp
@@ -2,16 +2,61 @@
 #include "PixelManager.h"
 
 namespace Imagina {
-	void Evaluator::SetReferenceLocation(const HPReal &x, const HPReal &y) {
+	class LowPrecisionEvaluator::LPRasterizingInterface : public IRasterizingInterface {
+		IRasterizingInterface &rasterizingInterface;
+		SRReal referenceX, referenceY;
+
+	public:
+		LPRasterizingInterface(IRasterizingInterface &rasterizingInterface, SRReal referenceX, SRReal referenceY)
+			: rasterizingInterface(rasterizingInterface), referenceX(referenceX), referenceY(referenceY) {}
+
+		virtual bool GetCoordinate(HRReal &x, HRReal &y) override;
+		virtual void WriteResults(SRReal Value) override;
+	};
+
+	bool LowPrecisionEvaluator::LPRasterizingInterface::GetCoordinate(HRReal &x, HRReal &y) {
+		bool result = rasterizingInterface.GetCoordinate(x, y);
+		x += referenceX;
+		y += referenceY;
+		return result;
+	}
+	void LowPrecisionEvaluator::LPRasterizingInterface::WriteResults(SRReal Value) {
+		rasterizingInterface.WriteResults(Value);
+	}
+
+	class LowPrecisionEvaluator::EvaluationTask : public ParallelTask/*, public ProgressTrackable*/ {
+		LowPrecisionEvaluator *evaluator;
+		IRasterizer *rasterizer;
+	public:
+		EvaluationTask(LowPrecisionEvaluator *evaluator, IRasterizer *rasterizer) : evaluator(evaluator), rasterizer(rasterizer) {}
+		//virtual std::string_view GetDescription() const override;
+		virtual void Execute() override;
+		//virtual bool GetProgress(SRReal &Numerator, SRReal &Denoninator) const override;
+	};
+
+	void LowPrecisionEvaluator::EvaluationTask::Execute() {
+		IRasterizingInterface &rasterizingInterface = rasterizer->GetRasterizingInterface();
+		LPRasterizingInterface lpRasterizingInterface(rasterizingInterface, evaluator->referenceX, evaluator->referenceY);
+		evaluator->Evaluate(lpRasterizingInterface);
+		rasterizer->FreeRasterizingInterface(rasterizingInterface);
+	}
+
+	ExecutionContext *LowPrecisionEvaluator::RunTaskForRectangle(const HRRectangle &rectangle, IRasterizer *rasterizer) {
+		if (currentExecutionContext) currentExecutionContext->WaitAndRelease();
+		currentExecutionContext = Computation::AddTask(new EvaluationTask(this, rasterizer));
+		currentExecutionContext->AddReference();
+		return currentExecutionContext;
+	}
+
+	void LowPrecisionEvaluator::SetReferenceLocation(const HPReal &x, const HPReal &y) {
 		referenceX = x;
 		referenceY = y;
 	}
-	void Evaluator::Evaluate(IRasterizer &rasterizer) {
-		IRasterizingInterface &ri = rasterizer.GetRasterizingInterface();
-	
+
+	void Evaluator::Evaluate(IRasterizingInterface &rasterizingInterface) {
 		HRReal x, y;
-		while (ri.GetCoordinate(x, y)) {
-			SRComplex c = { x + referenceX, y + referenceY };
+		while (rasterizingInterface.GetCoordinate(x, y)) {
+			SRComplex c = { x, y };
 			SRComplex z = 0.0;
 	
 			long i;
@@ -20,26 +65,7 @@ namespace Imagina {
 				if (norm(z) > 4.0) break;
 			}
 	
-			ri.WriteResults(i);
+			rasterizingInterface.WriteResults(i);
 		}
-		rasterizer.FreeRasterizingInterface(ri);
-	}
-	class Evaluator::EvaluationTask : public ParallelTask/*, public ProgressTrackable*/ {
-		Evaluator *evaluator;
-		IRasterizer *rasterizer;
-	public:
-		EvaluationTask(Evaluator *evaluator, IRasterizer *rasterizer) : evaluator(evaluator), rasterizer(rasterizer) {}
-		//virtual std::string_view GetDescription() const override;
-		virtual void Execute() override;
-		//virtual bool GetProgress(SRReal &Numerator, SRReal &Denoninator) const override;
-	};
-	void Evaluator::EvaluationTask::Execute() {
-		evaluator->Evaluate(*rasterizer);
-	}
-	ExecutionContext *Evaluator::RunTaskForRectangle(const HRRectangle &rectangle, IRasterizer *rasterizer) {
-		if (currentExecutionContext) currentExecutionContext->WaitAndRelease();
-		currentExecutionContext = Computation::AddTask(new EvaluationTask(this, rasterizer));
-		currentExecutionContext->AddReference();
-		return currentExecutionContext;
 	}
 }
--- include/Imagina/Evaluator.h
+++ include/Imagina/Evaluator.h
@@ -7,20 +7,32 @@
 namespace Imagina {
 	class IEvaluator {
 	public:
-		virtual void Evaluate(IRasterizer &rasterizer) = 0;
+		//virtual void Evaluate(IRasterizer &rasterizer) = 0;
 		virtual ExecutionContext *RunTaskForRectangle(const HRRectangle &rectangle, IRasterizer *rasterizer) = 0;
 	};
 
-	// TEMPORARY
-	class im_export Evaluator : public IEvaluator {
+	class StandardEvaluator : public IEvaluator {
+	public:
+		virtual void SetReferenceLocation(const HPReal &x, const HPReal &y) = 0;
+	};
+
+	class im_export LowPrecisionEvaluator : public StandardEvaluator {
+		class LPRasterizingInterface;
 		class EvaluationTask;
 
-		HPReal referenceX = 0.0, referenceY = 0.0;
+		SRReal referenceX = 0.0, referenceY = 0.0;
 		ExecutionContext *currentExecutionContext = nullptr;
 
 	public:
-		void SetReferenceLocation(const HPReal &x, const HPReal &y);
-		virtual void Evaluate(IRasterizer &rasterizer) override;
 		virtual ExecutionContext *RunTaskForRectangle(const HRRectangle &rectangle, IRasterizer *rasterizer) override;
+		virtual void SetReferenceLocation(const HPReal &x, const HPReal &y) override;
+
+		virtual void Evaluate(IRasterizingInterface &rasterizingInterface) = 0;
+	};
+
+	// TEMPORARY
+	class im_export Evaluator : public LowPrecisionEvaluator {
+	public:
+		virtual void Evaluate(IRasterizingInterface &rasterizingInterface) override;
 	};
 }
