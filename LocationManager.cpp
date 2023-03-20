c094d87183ac5058e251b9d95fc407ef519f1e86
Zhuoran <yuzhuoran13@gmail.com>
2023-03-20 12:52:13 +0800

    Dynamic precision

--- LocationManager.cpp
+++ LocationManager.cpp
@@ -1,5 +1,6 @@
 #include "LocationManager.h"
 #include "FractalContext.h"
+#include "FloatingPoint.h"
 
 namespace Imagina {
 	void StandardLocationManager::SetEvaluator(IEvaluator *evaluator) {
@@ -10,6 +11,11 @@ namespace Imagina {
 	}
 
 	void StandardLocationManager::LocationChanged(const HRLocation &location) {
+		BCInt precision = std::max<ExpInt>(32, -ExponentOf(location.HalfHeight)) + 32;
+
+		referenceX.SetPrecision(precision);
+		referenceY.SetPrecision(precision);
+
 		referenceX += location.X;
 		referenceY += location.Y;
 		static_cast<StandardEvaluator *>(evaluator)->SetReferenceLocation(referenceX, referenceY, location.HalfHeight); // TEMPORARY
