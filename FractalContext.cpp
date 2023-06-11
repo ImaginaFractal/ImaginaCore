d3d8beefd0e974f7106329967adf348adaac1fa6
Zhuoran <yuzhuoran13@gmail.com>
2023-06-12 00:08:38 +0800

    Remove dead code

--- FractalContext.cpp
+++ FractalContext.cpp
@@ -8,25 +8,16 @@ namespace Imagina {
 	}
 	void FractalContext::UsePixelManager(IPixelManager *pixelManager) {
 		PixelManager = pixelManager;
-
-		//if (Evaluator) PixelManager->SetEvaluator(Evaluator);
 	}
 	void FractalContext::UsePixelManager(IGpuPixelManager *pixelManager) {
-		//UsePixelManager(static_cast<IPixelManager *>(pixelManager));
 		PixelManager = pixelManager;
 		GpuTextureManager = pixelManager;
 	}
 	void FractalContext::UseEvaluator(IEvaluator *evaluator) {
 		Evaluator = evaluator;
-
-		//if (PixelManager) PixelManager->SetEvaluator(Evaluator);
-		//if (LocationManager) LocationManager->SetEvaluator(Evaluator);
 	}
 	void FractalContext::UseLocationManager(ILocationManager *locationManager) {
 		LocationManager = locationManager;
-		//locationManager->OnReferenceChange = std::bind_front(&FractalContext::UpdateRelativeCoordinate, this);
-		//
-		//if (Evaluator) LocationManager->SetEvaluator(Evaluator);
 	}
 	void FractalContext::Link() {
 		assert(Controller);
@@ -40,10 +31,10 @@ namespace Imagina {
 		LocationManager->SetEvaluator(Evaluator);
 		LocationManager->OnReferenceChange = std::bind_front(&FractalContext::UpdateRelativeCoordinate, this);
 	}
-	void FractalContext::SetTargetLocation(const HRLocation &location) {
-		PixelManager->SetTargetLocation(location);
-		LocationManager->LocationChanged(location);
-	}
+	//void FractalContext::SetTargetLocation(const HRLocation &location) {
+	//	PixelManager->SetTargetLocation(location);
+	//	LocationManager->LocationChanged(location);
+	//}
 
 	void FractalContext::UpdateRelativeCoordinate(HRReal differenceX, HRReal differenceY) {
 		Controller->UpdateRelativeCoordinate(differenceX, differenceY);
